9762fae6bce4c07a1ed74ca177f39efe785c0ea5
ipeterov <ipeterov1@gmail.com>
2023-01-21 14:33:44 +0400

    A somewhat working version

---
+++ parser.py
@@ -0,0 +1,140 @@
+import itertools
+import string
+
+
+class InvalidJSON(ValueError):
+    pass
+
+
+class UnexpectedSymbol(InvalidJSON):
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.message = f'Unexpected symbol "{symbol}"'
+        super().__init__(self.message)
+
+
+VALUE = '{["' + string.digits
+VALUE_OR_ARRAY_AND = VALUE + "]"
+KEY = '"'
+KEY_OR_OBJECT_END = KEY + "}"
+COMMA = ","
+COMMA_OR_ARRAY_END = COMMA + "]"
+COMMA_OR_OBJECT_END = COMMA + "}"
+COLON = ":"
+
+
+class JSONParser:
+    def __init__(self):
+        self.iterator = None
+
+    def parse(self, text: str):
+        self.iterator = text.__iter__()
+
+        try:
+            value = self.value_handler()
+        except StopIteration:
+            raise InvalidJSON()
+
+        try:
+            self.skip_whitespace(expected_symbols=[])
+        except StopIteration:
+            pass
+        else:
+            raise InvalidJSON()
+
+        return value
+
+    def value_handler(self):
+        symbol = self.skip_whitespace(VALUE)
+        if symbol == "{":
+            return self.object_handler()
+        if symbol == "[":
+            return self.array_handler()
+        if symbol == '"':
+            return self.string_handler()
+
+        self.push_back(symbol)
+        return self.number_handler()
+
+    def push_back(self, symbol):
+        self.iterator = itertools.chain([symbol], self.iterator)
+
+    def skip_whitespace(self, expected_symbols):
+        while symbol := next(self.iterator):
+            if symbol in string.whitespace:
+                continue
+
+            if symbol in expected_symbols:
+                return symbol
+
+            raise UnexpectedSymbol(symbol)
+
+    def string_handler(self):
+        letters = []
+        while symbol := next(self.iterator):
+            if symbol == '"':
+                break
+            letters.append(symbol)
+        return "".join(letters)
+
+    def number_handler(self):
+        digits = []
+        while symbol := next(self.iterator, None):
+            if symbol not in string.digits + ".":
+                self.push_back(symbol)
+                break
+            digits.append(symbol)
+        digits = "".join(digits)
+        if "." in digits:
+            return float(digits)
+        return int(digits)
+
+    def array_handler(self):
+        array = []
+        next_expecting = {
+            VALUE_OR_ARRAY_AND: COMMA_OR_ARRAY_END,
+            COMMA_OR_ARRAY_END: VALUE,
+            VALUE: COMMA_OR_ARRAY_END,
+        }
+
+        expecting = VALUE_OR_ARRAY_AND
+        while True:
+            symbol = self.skip_whitespace(expecting)
+
+            if symbol == "]":
+                return array
+
+            if expecting in {VALUE_OR_ARRAY_AND, VALUE}:
+                self.push_back(symbol)
+                array.append(self.value_handler())
+
+            expecting = next_expecting[expecting]
+
+    def object_handler(self):
+        obj = {}
+        next_expecting = {
+            KEY_OR_OBJECT_END: COLON,
+            COLON: VALUE,
+            VALUE: COMMA_OR_OBJECT_END,
+            COMMA_OR_OBJECT_END: KEY,
+            KEY: COLON,
+        }
+
+        expecting = KEY_OR_OBJECT_END
+        current_key = None
+        while True:
+            symbol = self.skip_whitespace(expecting)
+
+            if symbol == "}":
+                return obj
+
+            if expecting in {KEY, KEY_OR_OBJECT_END}:
+                current_key = self.string_handler()
+            elif expecting == VALUE:
+                self.push_back(symbol)
+                obj[current_key] = self.value_handler()
+
+            expecting = next_expecting[expecting]
+
+
+parser = JSONParser()
---
+++ test.py
@@ -0,0 +1,77 @@
+import pytest
+
+from parser import parser, InvalidJSON
+
+
+def check_example(source, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            parser.parse(source)
+    else:
+        assert parser.parse(source) == expected
+
+
+@pytest.mark.parametrize(
+    "source, expected",
+    [
+        ("8", 8),
+        ("0x42", InvalidJSON),
+    ],
+)
+def test_number(source, expected):
+    check_example(source, expected)
+
+
+@pytest.mark.parametrize(
+    "source, expected",
+    [
+        ('"qwerty"', "qwerty"),
+    ],
+)
+def test_string(source, expected):
+    check_example(source, expected)
+
+
+@pytest.mark.parametrize(
+    "source, expected",
+    [
+        ('["qwerty", "asd", 0]', ["qwerty", "asd", 0]),
+        ("[[[[]]]]", [[[[]]]]),
+        ("[[],[[]]]", [[], [[]]]),
+        ("]", InvalidJSON),
+        ("[[]]]", InvalidJSON),
+        ("[,1]", InvalidJSON),
+        ('[" ": 1]', InvalidJSON),
+        ("[1,\n1\n,1", InvalidJSON),
+    ],
+)
+def test_array(source, expected):
+    check_example(source, expected)
+
+
+@pytest.mark.parametrize(
+    "source, expected",
+    [
+        (
+            '{"qwerty": "asd", "foo": ["bar1", "bar2"]}',
+            {"qwerty": "asd", "foo": ["bar1", "bar2"]},
+        ),
+        (
+            '{"qwerty": "asd", "foo": {"bar": "baz"}}',
+            {"qwerty": "asd", "foo": {"bar": "baz"}},
+        ),
+        ('{"":0}', {"": 0}),
+        ('{"a":"b","a":"b"}', {"a": "b"}),
+        ("{1:1}", InvalidJSON),
+        ('{:"b"}', InvalidJSON),
+        ("{key: 'value'}", InvalidJSON),
+        ("{key: 'value'}", InvalidJSON),
+        ('{"x"::"b"}', InvalidJSON),
+        ('{"id":0,}', InvalidJSON),
+        ('{"id":0,,,,,}', InvalidJSON),
+        ('{"":', InvalidJSON),
+        ("{}}", InvalidJSON),
+    ],
+)
+def test_object(source, expected):
+    check_example(source, expected)
